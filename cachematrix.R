a407b4c0011d7635d40394429160b43d28e5a49d
charmgoggles <hogervorst.rm@gmail.com>
2014-11-22 13:58:46 +0100

    created a couple functions to solve a matrix and cache that solve.
    Part of Rprogramming 009 course.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## great inspiration from https://class.coursera.org/rprog-009/forum/thread?thread_id=457
+## Big help from poster Bill Hilton
 
 makeCacheMatrix <- function(x = matrix()) {
-
+#create matrix
+        m<-NULL 
+        get<-function() {x}
+        setSolve<-function(solve) {m<<-solve}
+        getSolve<-function() {m}
+        list(get=get, setSolve=setSolve, getSolve=getSolve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        #if already filled then return this.
+        #if not, solve(naam van matrix)
         ## Return a matrix that is the inverse of 'x'
+        m<- x$getSolve() #access the object x, and gest value of solve.
+        if(!is.null(m)){ # if solve was already cached (not NULL)
+        message(" getting cached data") 
+        return(m)  
+        }
+data<- x$get()
+m<-solve(data, ...) #if m was NULL data is solved.
+x$setSolve(m) # store calculated solve in x, return solve in code that called this function.
 }
